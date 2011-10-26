1bf90463b7b6903d81ad7d473f98c5ee83f41599
Zhao Cheng <zcbenz@gmail.com>
2011-10-26 11:40:03 +0000

    bbsindexd对

--- tool/bbsindexd.php
+++ tool/bbsindexd.php
@@ -3,18 +3,34 @@
 require_once 'search/lib/XS.php';
 require_once 'init.php';
 
+define('BBSLOG_POST', 7);
+define('BBSLOG_BM', 8);
+define('BBSLOG_DELETE', 9);
+define('BBSLOG_UPDATE', 10);
+
 $session = new Session();
 $session->initLogin();
 
 if (bbs_daemon('bbsindexd', true, true)) {
-    echo 'Cannot start bbsindexd';
     bbs_log('3error', 'bbsindexd already started');
 
     exit(0);
 }
 
 while ($msg = bbs_logrcv()) {
-    import_post($msg['board'], $msg['postid']);
+    if ($msg->mtype == BBSLOG_POST) {
+        import_post($msg['board'], $msg['postid']);
+    } else if ($msg->mtype == BBSLOG_DELETE) {
+        try {
+            $xs = new XS('sbbs');
+            $index = $xs->index;
+            $index->del($msg['pid']);
+        } catch(Exception $e) {
+            bbs_log('3error', 'bbsindexd remove index failed: ' . $e->getMessage());
+        }
+    } else if ($msg->mtype == BBSLOG_UPDATE) {
+        import_post($msg['mtext'], $msg['pid']);
+    }
 }
 
 function import_post($board_name, $id) {
@@ -40,13 +56,17 @@ function import_post($board_name, $id) {
             'board' => $board_name
         );
 
-        $xs = new XS('sbbs');
-        $index = $xs->index;
+        try {
+            $xs = new XS('sbbs');
+            $index = $xs->index;
 
-        $doc = new XSDocument;
-        $doc->setFields($data);
+            $doc = new XSDocument;
+            $doc->setFields($data);
 
-        $index->add($doc);
+            $index->update($doc);
+        } catch(Exception $e) {
+            bbs_log('3error', 'bbsindexd update index failed: ' . $e->getMessage());
+        }
     }
 }
 
