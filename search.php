47095f582bf37a4a355baacba4b2052d538149d2
Zhao Cheng <zcbenz@gmail.com>
2011-10-29 10:14:52 +0000

    过滤，以及标签

--- search.php
+++ search.php
@@ -2,9 +2,6 @@
 require_once 'search/lib/XS.php';
 require_once 'init.php';
 
-$session = new Session();
-$session->initLogin();
-
 Lib::load(array('utils/pagination.php'));
 
 // 支持的 GET 参数列表
@@ -21,40 +18,36 @@ Lib::load(array('utils/pagination.php'));
 $eu = '';
 $__ = array('q', 'm', 'f', 's', 'p', 'ie', 'oe', 'xml');
 foreach ($__ as $_)
-	$$_ = isset($_GET[$_]) ? $_GET[$_] : '';
+    $$_ = isset($_GET[$_]) ? $_GET[$_] : '';
 
 // input encoding
 if (!empty($ie) && !empty($q) && strcasecmp($ie, 'GBK'))
 {
-	$q = XS::convert($q, $cs, $ie);
-	$eu .= '&ie=' . $ie;
+    $q = XS::convert($q, $cs, $ie);
+    $eu .= '&ie=' . $ie;
 }
 
 // output encoding
 if (!empty($oe) && strcasecmp($oe, 'GBK'))
 {
 
-	function xs_output_encoding($buf)
-	{
-		return XS::convert($buf, $GLOBALS['oe'], 'GBK');
-	}
-	ob_start('xs_output_encoding');
-	$eu .= '&oe=' . $oe;
+    function xs_output_encoding($buf)
+    {
+        return XS::convert($buf, $GLOBALS['oe'], 'GBK');
+    }
+    ob_start('xs_output_encoding');
+    $eu .= '&oe=' . $oe;
 }
 else
 {
-	$oe = 'GBK';
+    $oe = 'GBK';
 }
 
 // recheck request parameters
 $q = get_magic_quotes_gpc() ? stripslashes($q) : $q;
-$f = empty($f) ? '_all' : $f;
-${'m_check'} = ($m == 'yes' ? ' checked' : '');
-${'f_' . $f} = ' checked';
-${'s_' . $s} = ' selected';
 
 // base url
-$bu = '/s?q=' . urlencode($_GET['q']) . '&m=' . $m . '&f=' . $f . '&s=' . $s . $eu;
+$bu = '/s?q=' . urlencode($_GET['q']) . $eu;
 
 // other variable maybe used in tpl
 $count = $total = $search_cost = 0;
@@ -65,62 +58,50 @@ $total_begin = microtime(true);
 // perform the search
 try
 {
-	$xs = new XS('sbbs');
-	$search = $xs->search;
-	$search->setCharset('GBK');
+    $xs = new XS('sbbs');
+    $search = $xs->search;
+    $search->setCharset('GBK');
 
-	if (empty($q))
-	{
-		// just show hot query
-		$hot = $search->getHotQuery(10);
-	}
-	else
-	{
-		// fuzzy search
-		$search->setFuzzy($m === 'yes');
-		
-		// set query
-		if (!empty($f) && $f != '_all')
-		{
-			$search->setQuery($f . ':(' . $q . ')');
-		}
-		else
-		{
-			$search->setQuery($q);
-		}
+    if (empty($q))
+    {
+        // just show hot query
+        $hot = $search->getHotQuery(10);
+    }
+    else
+    {
+        // fuzzy search
+        $search->setFuzzy(false);
 
-		// set sort
-		if (($pos = strrpos($s, '_')) !== false)
-		{
-			$sf = substr($s, 0, $pos);
-			$st = substr($s, $pos + 1);
-			$search->setSort($sf, $st === 'ASC');
-		}
+        // set query
+        $search->setQuery($q);
 
-		// set offset, limit
-		$p = max(1, intval($p));
-		$n = 10;
-		$search->setLimit($n, ($p - 1) * $n);
+        // filter private posts
+        $search->addRange('access', 1, 1);
 
-		// get the result
-		$search_begin = microtime(true);
-		$docs = $search->search();
-		$search_cost = microtime(true) - $search_begin;
+        // set offset, limit
+        $p = max(1, intval($p));
+        $n = 10;
+        $search->setLimit($n, ($p - 1) * $n);
 
-		// get other result
-		$count = $search->getLastCount();
-		$total = $search->getDbTotal();
+        // get the result
+        $search_begin = microtime(true);
+        $docs = $search->search();
+        $search_cost = microtime(true) - $search_begin;
+
+        // get other result
+        $count = $search->getLastCount();
+        $total = $search->getDbTotal();
 
         // try to corrected, if resul too few
         if ($count < 1 || $count < ceil(0.001 * $total))
-            $corrected = $search->getCorrectedQuery();			
+            $corrected = $search->getCorrectedQuery();
         // get related query
-        $related = $search->getRelatedQuery();			
-	}
+        $related = $search->getRelatedQuery();
+    }
 }
 catch (XSException $e)
 {
-	$error = strval($e);
+    $error = strval($e);
 }
 
 // calculate total time cost
