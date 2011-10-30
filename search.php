7aba04ff42c64bf9d7f1aee69e403608f6ed8432
Zhao Cheng <zcbenz@gmail.com>
2011-10-30 03:09:08 +0000

    logo, 搜索结果过滤，时间范围，结果排序

--- search.php
+++ search.php
@@ -7,24 +7,30 @@ Lib::load(array('utils/pagination.php'));
 // 支持的 GET 参数列表
 // q: 查询语句
 // m: 开启模糊搜索，其值为 yes/no
-// f: 只搜索某个字段，其值为字段名称，要求该字段的索引方式为 self/both
-// s: 排序字段名称及方式，其值形式为：xxx_ASC 或 xxx_DESC
+// f: 只显示主题贴
+// a: 只显示带附件的帖子
+// g: 只显示精华贴
+// t: 过去xx内
+// s: 排序方式
 // p: 显示第几页，每页数量为 XSSearch::PAGE_SIZE 即 10 条
 // ie: 查询语句编码，默认为 GBK
 // oe: 输出编码，默认为 GBK
-// xml: 是否将搜索结果以 XML 格式输出，其值为 yes/no
 //
 // variables
+$attr = array();
 $eu = '';
-$__ = array('q', 'm', 'f', 's', 'p', 'ie', 'oe', 'xml');
+$__ = array('q', 'm', 'f', 'a', 'g', 't', 's', 'p', 'ie', 'oe');
 foreach ($__ as $_)
     $$_ = isset($_GET[$_]) ? $_GET[$_] : '';
+
+// base url
+$bu = '/s?';
 
 // input encoding
 if (!empty($ie) && !empty($q) && strcasecmp($ie, 'GBK'))
 {
     $q = XS::convert($q, $cs, $ie);
-    $eu .= '&ie=' . $ie;
+    $attr['ie'] = $ie;
 }
 
 // output encoding
@@ -36,7 +42,7 @@ if (!empty($oe) && strcasecmp($oe, 'GBK'))
         return XS::convert($buf, $GLOBALS['oe'], 'GBK');
     }
     ob_start('xs_output_encoding');
-    $eu .= '&oe=' . $oe;
+    $attr['oe'] = $oe;
 }
 else
 {
@@ -45,9 +51,7 @@ else
 
 // recheck request parameters
 $q = get_magic_quotes_gpc() ? stripslashes($q) : $q;
-
-// base url
-$bu = '/s?q=' . urlencode($_GET['q']) . $eu;
+$attr['q'] = $q;
 
 // other variable maybe used in tpl
 $count = $total = $search_cost = 0;
@@ -78,6 +82,41 @@ try
         // filter private posts
         $search->addRange('access', 1, 1);
 
+        // custom filters
+        if ($f) {
+            $search->addRange('first', 1, 1);
+            $attr['f'] = 1;
+        }
+        if ($a) {
+            $search->addRange('attachment', 1, 1);
+            $attr['a'] = 1;
+        }
+        if ($g) {
+            $search->addRange('good', 1, 1);
+            $attr['g'] = 1;
+        }
+
+        // time ranges
+        if ($t) {
+            define('DAY', 24 * 3600);
+            $diff = array(2 * DAY, 7 * DAY, 30 * DAY, 365 * DAY);
+            $now = time();
+            if (0 < $t && $t < 5) {
+                $search->addRange('time', date('Ymd', $now - $diff[$t - 1]), date('Ymd'));
+                $attr['t'] = $t;
+            }
+        }
+
+        // sort?
+        if ($s) {
+            if ($s == 1)
+                $search->setSort('time');
+            else
+                $search->setSort('time', true);
+
+            $attr['s'] = $s;
+        }
+
         // set offset, limit
         $p = max(1, intval($p));
         $n = 10;
@@ -95,6 +134,7 @@ try
         // try to corrected, if resul too few
         if ($count < 1 || $count < ceil(0.001 * $total))
             $corrected = $search->getCorrectedQuery();
+
         // get related query
         $related = $search->getRelatedQuery();
     }
@@ -107,8 +147,36 @@ catch (XSException $e)
 // calculate total time cost
 $total_cost = microtime(true) - $total_begin;
 
-if ($count > $n)
-    $pager = paginate_three($bu, $p, (int)($count / $n) + 1, 4);
+if ($count > $n) {
+    $reload = $bu . http_build_query($attr);
+    $pager = paginate_three($reload, $p, (int)($count / $n) + 1, 4);
+
+    if ($p > 0) $attr['p'] = $p;
+}
+
+// helper for generate filter link
+function switch_attr($bu, $attr, $f) {
+    if ($attr[$f]) {
+        unset ($attr[$f]);
+        return 'class="active" href="'. $bu . http_build_query($attr) . '"';
+    } else {
+        $attr[$f] = 1;
+        return 'href="'. $bu . http_build_query($attr) . '"';
+    }
+}
+
+function value_attr($bu, $attr, $f, $v) {
+    $r = '';
+    if ($v == $attr[$f]) $r = 'class="active" ';
+
+    if ($v == 0) {
+        unset ($attr[$f]);
+        return $r . 'href="'. $bu . http_build_query($attr) . '"';
+    } else {
+        $attr[$f] = $v;
+        return $r . 'href="'. $bu . http_build_query($attr) . '"';
+    }
+}
 
 // output the data
 include dirname(__FILE__) . '/search.html';
